8532468335cd171602039d0cc8b504f36eb277e5
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-03-29 10:19:31 -0600

    📅  | Tue Mar 29 10:19:31 MDT 2022

--- minewalker/src/app/board/board.component.ts
+++ minewalker/src/app/board/board.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { BoardService } from '../services/board.service';
+import { Coordinate } from './square/square.component';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.css']
+  styleUrls: ['./board.component.css'],
 })
 export class BoardComponent implements OnInit {
-
-  constructor() { }
+  coordinate: Coordinate = { x: 1, y: 1 };
+  coordintes: Coordinate[] | undefined;
+  constructor(public boardService: BoardService) {}
 
   ngOnInit(): void {
+    this.coordintes = this.boardService.getCoordinates();
+    console.log(JSON.stringify(this.coordintes));
   }
-
 }
--- minewalker/src/app/board/square/square.component.ts
+++ minewalker/src/app/board/square/square.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+export type Coordinate = {
+  x: number;
+  y: number;
+};
 @Component({
   selector: 'app-square',
   templateUrl: './square.component.html',
-  styleUrls: ['./square.component.css']
+  styleUrls: ['./square.component.css'],
 })
 export class SquareComponent implements OnInit {
+  @Input() public coordinate: Coordinate | undefined;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
