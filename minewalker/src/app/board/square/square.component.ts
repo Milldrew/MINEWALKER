b9ec67a32313c09d6317ff2884c1e3072b3e7c8b
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-03-30 09:59:38 -0600

    📅  | Wed Mar 30 09:59:38 MDT 2022

--- minewalker/src/app/board/square/square.component.ts
+++ minewalker/src/app/board/square/square.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { SquareService } from 'src/app/services/square.service';
+import { UserService } from 'src/app/services/user.service';
 export type Coordinate = {
   x: number;
   y: number;
@@ -11,16 +12,21 @@ export type Coordinate = {
 })
 export class SquareComponent implements OnInit {
   @Input() public coordinate: Coordinate | undefined;
-  constructor(public squareService: SquareService) {}
+  constructor(
+    public userService: UserService,
+    public squareService: SquareService
+  ) {}
   public squareClasses: any;
   isHappy: any;
   isMine: any;
   hasUser: boolean = true;
-
+  move(coordinate: Coordinate | undefined) {
+    this.userService.moveUser(coordinate);
+  }
   ngOnInit(): void {
+    this.hasUser = this.userService.hasUser(this.coordinate);
     this.isHappy = this.squareService.isOnHappyPath(this.coordinate);
     this.isMine = this.squareService.isMine(this.coordinate);
-    console.log(this.isMine);
     this.squareClasses = {
       happy: this.isHappy,
       mine: this.isMine,
