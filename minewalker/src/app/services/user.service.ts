c33720bd5e5dc69cadf9a43fd611db3945677d84
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-03-30 09:53:25 -0600

    📅  | Wed Mar 30 09:53:25 MDT 2022

--- minewalker/src/app/services/user.service.ts
+++ minewalker/src/app/services/user.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Coordinate } from '../board/square/square.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  usersCoordinate: Coordinate = { x: 1, y: 1 };
+  constructor() {}
 
-  constructor() { }
+  hasUser(coordinate: Coordinate | undefined) {
+    console.log('from user service', coordinate);
+
+    if (coordinate) {
+      return (
+        this.usersCoordinate.x === coordinate.x &&
+        this.usersCoordinate.y === coordinate.y
+      );
+    }
+    return false;
+  }
 }
