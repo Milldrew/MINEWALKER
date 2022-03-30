0b79fc48eeaebe9062d18b4b9dd3e4342c97b2ed
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-03-30 11:59:56 -0600

    📅  | Wed Mar 30 11:59:56 MDT 2022

--- minewalker/src/app/winner-prompt/winner-prompt.component.ts
+++ minewalker/src/app/winner-prompt/winner-prompt.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-winner-prompt',
   templateUrl: './winner-prompt.component.html',
-  styleUrls: ['./winner-prompt.component.css']
+  styleUrls: ['./winner-prompt.component.css'],
 })
 export class WinnerPromptComponent implements OnInit {
+  constructor(public userService: UserService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  playAgain() {
+    console.log('Play Again');
+    this.userService.setHasWon(false);
   }
-
+  restart() {
+    console.log('restart');
+    window.location.reload();
+  }
 }
