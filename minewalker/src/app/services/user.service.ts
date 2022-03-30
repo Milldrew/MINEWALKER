4d05f4b30f56e6395fad13db42a858b3e2125770
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-03-30 11:20:43 -0600

    📅  | Wed Mar 30 11:20:43 MDT 2022

--- minewalker/src/app/services/user.service.ts
+++ minewalker/src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { of, Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Coordinate } from '../board/square/square.component';
+import { GameService } from './game.service';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +9,7 @@ import { Coordinate } from '../board/square/square.component';
 export class UserService {
   usersCoordinate: undefined | Coordinate = { x: 1, y: 1 };
   usersCoordinate$: undefined | Observable<Coordinate> = of({ x: 1, y: 1 });
-  constructor() {}
+  constructor(public gameService: GameService) {}
 
   hasUser(coordinate: Coordinate | undefined) {
     if (coordinate && this.usersCoordinate) {
@@ -23,5 +24,14 @@ export class UserService {
     console.log(coordinate);
     this.usersCoordinate = coordinate;
     console.log(this.usersCoordinate);
+  }
+
+  hasWonRound(coordinate: Coordinate) {
+    const boardSize = this.gameService.boardSize;
+    console.log('from win round', boardSize);
+    return coordinate.x === boardSize && coordinate.y === boardSize;
+  }
+  winRound(coordinate: Coordinate) {
+    console.log(`WON ROUND`);
   }
 }
