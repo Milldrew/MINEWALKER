b66baa215ded0a3bcc134f59450bb478a93e2d22
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-03-30 12:06:09 -0600

    📅  | Wed Mar 30 12:06:09 MDT 2022

--- minewalker/src/app/winner-prompt/winner-prompt.component.ts
+++ minewalker/src/app/winner-prompt/winner-prompt.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BoardService } from '../services/board.service';
+import { GameService } from '../services/game.service';
+import { MinesService } from '../services/mines.service';
 import { UserService } from '../services/user.service';
 
 @Component({
@@ -7,13 +10,20 @@ import { UserService } from '../services/user.service';
   styleUrls: ['./winner-prompt.component.css'],
 })
 export class WinnerPromptComponent implements OnInit {
-  constructor(public userService: UserService) {}
+  constructor(
+    public userService: UserService,
+    public minesService: MinesService,
+    public boardService: BoardService
+  ) {}
 
   ngOnInit(): void {}
 
   playAgain() {
     console.log('Play Again');
     this.userService.setHasWon(false);
+    this.userService.moveUser({ x: 1, y: 1 });
+    this.boardService.makeHappyPath();
+    this.minesService.layMines();
   }
   restart() {
     console.log('restart');
