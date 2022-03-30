3a33abfc444d2c861ee2d1db7a62f5de7816ea21
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-03-30 15:28:40 -0600

    📅  | Wed Mar 30 15:28:40 MDT 2022

--- minewalker/src/app/services/score.service.ts
+++ minewalker/src/app/services/score.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ScoreService {
+  lives: number = 10;
+  currentScore: number = 100;
+  totalScore: number = 0;
+  constructor() {}
 
-  constructor() { }
+  getScoreData() {
+    return of({
+      lives: this.lives,
+      currentScore: this.currentScore,
+      totalScore: this.totalScore,
+    });
+  }
+
+  winRoundIncrease() {
+    console.log('HELLO');
+    this.totalScore += this.currentScore;
+    console.log(this.totalScore);
+    this.getScoreData();
+  }
 }
