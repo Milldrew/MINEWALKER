0ec662c9a6c6b6061457b453824c371ca8188ec0
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-03-30 15:28:43 -0600

    📅  | Wed Mar 30 15:28:43 MDT 2022

--- minewalker/src/app/user-interface/scoreboard/scoreboard.component.ts
+++ minewalker/src/app/user-interface/scoreboard/scoreboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ScoreService } from 'src/app/services/score.service';
 
 @Component({
   selector: 'app-scoreboard',
@@ -10,7 +11,14 @@ export class ScoreboardComponent implements OnInit {
   currentScore = 100;
   totalScore = 10000;
 
-  constructor() {}
+  constructor(public scoreService: ScoreService) {}
 
+  ngAfterContentChecked() {
+    this.scoreService.getScoreData().subscribe((data) => {
+      this.lives = data.lives;
+      this.currentScore = data.currentScore;
+      this.totalScore = data.totalScore;
+    });
+  }
   ngOnInit(): void {}
 }
